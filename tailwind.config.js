5bac56c1e0d2d70f677509cdb99ba5a0034caf40
Akimov Alexey <98049437+aiakimov@users.noreply.github.com>
2022-09-28 12:53:00 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': 'Montserrat, sans-serif',
+      }
+    },
+  },
+  plugins: [],
+}
